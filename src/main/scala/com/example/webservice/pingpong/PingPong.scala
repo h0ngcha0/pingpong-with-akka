8dd1f1ad44b282f86a7ada9b270d1ad5b82f9b75
hongchao <lbs.lhc@gmail.com>
2016-09-28 00:35:08 +0200

    persisted pingpong

--- src/main/scala/com/example/webservice/pingpong/PingPong.scala
+++ src/main/scala/com/example/webservice/pingpong/PingPong.scala
@@ -4,13 +4,16 @@ import akka.actor.{Actor, ActorSelection, Address, Props, RootActorPath}
 import akka.cluster.{ Cluster, MemberStatus }
 import akka.event.Logging
 import akka.pattern.ask
+import akka.persistence.PersistentActor
 import akka.util.Timeout
+import java.net.InetAddress
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-class ClusteredPingPong extends Actor {
+
+class PingPong extends PersistentActor {
   val log = Logging(context.system, this)
 
   implicit val executionContext = context.dispatcher
@@ -19,29 +22,40 @@ class ClusteredPingPong extends Actor {
   val cluster = Cluster.get(context.system)
 
   var ballsSeen = 0
-
-  def receive: Receive = {
-    case PingPongball => ballsSeen += 1; sender ! PingPongball
-    case BallsSeen    => sender ! Status(s"seen $ballsSeen balls")
+  override def persistenceId: String = InetAddress.getLocalHost.getHostName
+  override def receiveCommand: Receive = {
+    case msg @ PingPongball => persist(msg) { m =>
+      ballsSeen += 1; sender ! PingPongball
+    }
+    case BallsSeen          => sender ! Status(s"seen $ballsSeen balls")
 
     case BallsSeenAll       =>
       val nodeAddrs = cluster.state.members.filter(_.status == MemberStatus.Up).map(_.address).toSeq
       val responses = nodeAddrs.map { addr =>
         val nodePath = RootActorPath(addr)
-        val actor = context.actorSelection(nodePath / "user" / "ClusteredPingPong")
+        val actor = context.actorSelection(nodePath / "user" / "PingPong")
         log.info(s"sending to $actor")
-        (actor ? BallsSeen).mapTo[Status]
+        (actor ? BallsSeen).mapTo[Status].map { status =>
+          status.copy(status = s"$addr has seen ${status.status}.")
+        }
       }
 
       val s = sender
       Future.sequence(responses).onComplete {
-        case Success(statuses) => s ! Status(statuses map (_.status) mkString "\n")
+        case Success(statuses) => s ! Status(statuses map (_.status) mkString " ||| ")
         case Failure(ex)       => s ! Status(ex.toString)
       }
 
   }
+
+  override def receiveRecover: Receive = {
+    case PingPongball =>
+      ballsSeen = ballsSeen + 1
+      log.info("Replayed PingPongball")
+  }
+
 }
 
-object ClusteredPingPong {
-  def props: Props = Props[ClusteredPingPong]
+object PingPong {
+  def props: Props = Props[PingPong]
 }
