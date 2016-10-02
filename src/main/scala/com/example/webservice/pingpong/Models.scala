4db91dbfd26c5d2b59e450a37fa0f9d53285439e
hongchao <lbs.lhc@gmail.com>
2016-10-03 00:53:36 +0200

    add supervised pingpong back

--- src/main/scala/com/example/webservice/pingpong/Models.scala
+++ src/main/scala/com/example/webservice/pingpong/Models.scala
@@ -1,33 +1,14 @@
 package com.example.webservice.pingpong
 
-import spray.json._
-
 trait Payload
 trait Ball extends Payload
 case object PingPongball extends Ball
-case class Status(status: String) extends Payload
-
-case object BallsSeen
-case object BallsSeenAll
+case object Basketball extends Ball
+case object Fireball extends Ball
+case object Bullet extends Ball
+case class Status(status: String, from: Option[String] = None) extends Payload
+case object BallsSeen extends Payload
 
 class Killed extends Exception    // stop
 class Faint extends Exception     // resumable
 class Injured extends Exception   // recoverable
-
-trait Protocols extends DefaultJsonProtocol {
-  implicit val payloadFormat: RootJsonFormat[Payload] = new RootJsonFormat[Payload] {
-    def read(json: JsValue): Payload = json match {
-      case obj: JsObject => obj.fields.head match {
-        case ("type",   JsString("pingpongball"))  => PingPongball
-        case ("status", JsString(label))           => Status(label)
-        case _ => deserializationError("expecting (kind, JsString), got " + json)
-      }
-      case _ => deserializationError("expecting JsString, got " + json)
-    }
-
-    def write(payload: Payload): JsValue = payload match {
-      case PingPongball  => JsObject("type"   -> JsString("pingpongball"))
-      case Status(label) => JsObject("status" -> JsString(label))
-    }
-  }
-}
--- src/main/scala/com/example/webservice/pingpong/PingPong.scala
+++ src/main/scala/com/example/webservice/pingpong/PingPong.scala
@@ -8,16 +8,11 @@ class PingPong extends Actor {
   val log = Logging(context.system, this)
 
   var ballsSeen = 0
-  override def preRestart(reason: Throwable, message: Option[Any]) = {
-    log.info(s"pingpong is restarted because of $reason")
-  }
 
   def receive: Receive = {
     case PingPongball => ballsSeen = ballsSeen+1; sender ! PingPongball
     case BallsSeen    => sender ! Status(s"seen $ballsSeen balls")
-    case msg @ _      => throw new Exception(s"recieved unknown message $msg")
   }
-
 
 }
 
